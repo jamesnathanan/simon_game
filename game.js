e3afd45ac47ea37b51ead5e3d152585dd004fd24
jiran <jiranathanan@gmail.com>
2021-01-20 12:23:08 +0700

    add startover function

--- game.js
+++ game.js
@@ -65,5 +65,13 @@ function checkAnswer(currentLevel) {
         setTimeout(function() {
             $("body").removeClass("game-over");
         }, 200);
+        $("h1").text("Game Over, Press Any Key to Restart");
+        startOver();
     }
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
 }
