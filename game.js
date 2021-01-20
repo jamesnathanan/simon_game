0029eea640b2fc0aea6fa8721baf1d5c5f224f79
jiran <jiranathanan@gmail.com>
2021-01-20 09:47:04 +0700

    add all files

---
+++ game.js
@@ -0,0 +1,20 @@
+const buttonColours = ["red", "blue", "green", "yellow"];
+const gamePattern = [];
+const userClickedPattern = [];
+
+$(".btn").click(function() {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+});
+
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 3) + 1;
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeOut(100);
+    var audio = new Audio("sounds/"+randomChosenColour+".mp3");
+    audio.play();
+}
